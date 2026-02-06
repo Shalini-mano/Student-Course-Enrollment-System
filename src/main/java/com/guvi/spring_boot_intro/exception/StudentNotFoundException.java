c664b54bb406e64f74dab58eb8dcd14ca9b9bad1
Shalini mano <shalinimano2595@gmail.com>
2026-02-06 17:07:59 +0100

    Tracking student management system

---
+++ src/main/java/com/guvi/spring_boot_intro/exception/StudentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.guvi.spring_boot_intro.exception;
+
+import java.util.UUID;
+
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String id) {
+
+        super("Student Not Found: " + id);
+    }
+}
